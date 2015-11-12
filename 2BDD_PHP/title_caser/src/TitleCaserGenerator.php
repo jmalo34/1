c3b58cf8389c93497916af210c8e5d557195291a
Jenna Maloney <jmalo@uw.edu>
2015-11-12 12:38:12 -0800

    make first 4 tests pass

--- 2BDD_PHP/title_caser/src/TitleCaserGenerator.php
+++ 2BDD_PHP/title_caser/src/TitleCaserGenerator.php
@@ -3,13 +3,19 @@
     {
         function makeTitleCaser($input_title)
         {
-            $input_array_of_words = explode(" ", $input_title);
+            $input_array_of_words = explode(" ", strtolower($input_title));
             $output_titlecased = array();
+            $excluded = array('and', 'as', 'at', 'but', 'for', 'from', 'if', 'in', 'into', 'like', 'near', 'nor', 'of', 'off', 'on', 'once', 'onto', 'or', 'out', 'over', 'past', 'so', 'than', 'that', 'the', 'til', 'to', 'up', 'upon', 'with', 'when', 'yet');
             foreach ($input_array_of_words as $word)
-            {
-                array_push($output_titlecased, ucfirst($word));
-            }
-            return implode(" ", $output_titlecased);
+                if (in_array($word, $excluded))
+                {
+                    array_push($output_titlecased, $word);
+                }
+                else
+                {
+                    array_push($output_titlecased, ucfirst($word));
+                }
+            return ucfirst(implode(" ", $output_titlecased));
         }
     }
  ?>
--- 2BDD_PHP/title_caser/tests/TitleCaserGeneratorTest.php
+++ 2BDD_PHP/title_caser/tests/TitleCaserGeneratorTest.php
@@ -41,5 +41,20 @@
             //Assert
             $this->assertEquals("101 Dalmatians & Friends", $result);
         }
+//is this "numeric" test redundant, since it is only testing that the Title Caser is to leave the input as-is if it meets the conditions of a non-alphabetic character?
+
+        function test_makeTitleCaser_exclusions()
+        {
+            //Arrange
+            $test_TitleCaserGenerator = new TitleCaserGenerator;
+            $input = "the princess and the pea";
+
+            //Act
+            $result = $test_TitleCaserGenerator->makeTitleCaser($input);
+
+            //Assert
+            $this->assertEquals("The Princess and the Pea", $result);
+        }
+
     }
  ?>
