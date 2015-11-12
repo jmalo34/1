d88e7d8a67f895ed40d6a64aa27c34344a2086a4
Jenna Maloney <jmalo@uw.edu>
2015-11-12 12:45:59 -0800

    display src code in a more readable way

--- 2BDD_PHP/title_caser/src/TitleCaserGenerator.php
+++ 2BDD_PHP/title_caser/src/TitleCaserGenerator.php
@@ -4,8 +4,11 @@
         function makeTitleCaser($input_title)
         {
             $input_array_of_words = explode(" ", strtolower($input_title));
+
             $output_titlecased = array();
+
             $excluded = array('and', 'as', 'at', 'but', 'for', 'from', 'if', 'in', 'into', 'like', 'near', 'nor', 'of', 'off', 'on', 'once', 'onto', 'or', 'out', 'over', 'past', 'so', 'than', 'that', 'the', 'til', 'to', 'up', 'upon', 'with', 'when', 'yet');
+
             foreach ($input_array_of_words as $word)
                 if (in_array($word, $excluded))
                 {
