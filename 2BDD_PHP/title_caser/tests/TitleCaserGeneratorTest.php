66ec3637a918672f5006eaea78f0a3b83b942fe6
Jenna Maloney <jmalo@uw.edu>
2015-11-12 12:04:29 -0800

    make first 3 tests pass

--- 2BDD_PHP/title_caser/tests/TitleCaserGeneratorTest.php
+++ 2BDD_PHP/title_caser/tests/TitleCaserGeneratorTest.php
@@ -28,5 +28,18 @@
             //Assert
             $this->assertEquals("The Little Mermaid", $result);
         }
+
+        function test_makeTitleCaser_numeric()
+        {
+            //Arrange
+            $test_TitleCaserGenerator = new TitleCaserGenerator;
+            $input = "101 dalmatians & friends";
+
+            //Act
+            $result = $test_TitleCaserGenerator->makeTitleCaser($input);
+
+            //Assert
+            $this->assertEquals("101 Dalmatians & Friends", $result);
+        }
     }
  ?>
